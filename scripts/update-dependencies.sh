3afef54476e30308092b6064eb182368c8a4b890
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2024-03-30 16:25:18 +0100

    update dependencies

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env sh
-npx npm-check-updates -f npm-check-updates -u
-npx npm-check-updates -f @types/node -t minor -u
-npx npm-check-updates -x @types/node -u
-npm install
-npm update
+pnpm exec npm-check-updates -x @types/node -u
+pnpm install
+pnpm update
